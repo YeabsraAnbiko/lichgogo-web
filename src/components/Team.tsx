742c8e8c327bc61ddfca12775796ea27eb61bf46
Yeabsra <anbikoyeabsra@gmail.com>
2025-06-06 03:16:19 +0300

    Socials props fix

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 
 interface SocialLink {
-  platform: 'github' | 'linkedin' | 'twitter';
+  platform: 'github' | 'linkedin' | 'twitter' | 'facebook' | 'instagram';
   url: string;
 }
 
